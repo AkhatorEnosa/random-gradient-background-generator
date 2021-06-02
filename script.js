cbb33b34dfd3f1309c600173f1a6dc0e20a29fe6
AkhatorEnosa <aosakhogba@gmail.com>
2021-06-02 10:55:21 +0100

    Added main files

---
+++ script.js
@@ -0,0 +1,81 @@
+var body = document.getElementById("gradient");
+var circle = document.querySelector(".circle");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var colVal1 = document.querySelector(".color-1");
+var colVal2= document.querySelector(".color-2");
+var button = document.querySelector(".button");
+var button1 = document.querySelector(".button1");
+var button2 = document.querySelector(".button2");
+var hiddenInput = document.querySelector(".copy");
+var randomValue = "abcdef1234567890";
+
+function getAndSetGradient() {
+	circle.style.background = "linear-gradient(to right, "+ color1.value + ", "+ color2.value + ")";
+	hiddenInput.value = circle.style.background;
+}
+
+function setRandomValue1(){
+	var shuffled = randomValue.split('').sort(function(){
+		return 0.5-Math.random()
+	}).join('');
+	
+	 return shuffled.substring(0,6);
+}
+
+function setRandomValue2(){
+	var shuffled = randomValue.split('').sort(function(){
+		return 0.5-Math.random()
+	}).join('');
+	
+	 return shuffled.substring(0,6);
+}
+
+function newGradientValue(){
+	color1.value = "#" + setRandomValue1();
+	color2.value = "#" + setRandomValue2();
+	colVal1.textContent = color1.value;
+	colVal2.textContent = color2.value;
+	getAndSetGradient();
+}
+
+function addBackground() {
+	body.style.background = "linear-gradient(to right, "+ color1.value + ", "+ color2.value + ")";
+	document.querySelector("h1").style.color = "white";
+	colVal1.style.color = "white";
+	colVal2.style.color = "white";
+	button.style.border = "none";
+	button1.style.border = "none";
+	button2.style.border = "none";
+}
+
+function copyText(){
+	
+	/* Select the text field */
+	hiddenInput.select();
+	hiddenInput.setSelectionRange(0, 99999); /*For mobile devices*/
+	
+	/* Copy the text inside the text field */
+	if (document.execCommand("copy")) {
+		console.log(true)
+	} else {
+		console.log(false)
+	}
+	
+	/* Alert the copied text */
+	alert("Gradient background color: " + hiddenInput.value + " copied to clipboard!");
+}
+
+window.addEventListener("load", getAndSetGradient);
+
+window.addEventListener("load", newGradientValue);
+
+button.addEventListener("click", newGradientValue);
+
+button1.addEventListener("click", addBackground);
+
+button2.addEventListener("click", copyText);
+
+color1.addEventListener("input", getAndSetGradient);
+
+color2.addEventListener("input", getAndSetGradient);
